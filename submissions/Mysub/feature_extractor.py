cf67c0cf517d5bdcb5bcd3c84a947be3e19124bb
samarche92 <samar.chehade.sc@gmail.com>
2017-12-05 16:36:45 +0100

    ça ne marche pas mais l'idee est la

--- submissions/Mysub/feature_extractor.py
+++ submissions/Mysub/feature_extractor.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 class FeatureExtractor():
@@ -8,6 +9,7 @@ class FeatureExtractor():
     def fit(self, X_df, y):
         pass
 
+
     def transform(self, X_df):
         X_df_new = pd.concat(
             [X_df.get(['Fare', 'Age', 'SibSp', 'Parch']),
@@ -16,6 +18,19 @@ class FeatureExtractor():
              pd.get_dummies(
                  X_df.Embarked, prefix='Embarked', drop_first=True)],
             axis=1)
-        X_df_new = X_df_new.fillna(-1)
+        
+        table = pd.pivot_table(X_df_new,values='Age', index='Parch' ,columns='SibSp', aggfunc=np.median)
+# Define function to return value of this pivot_table
+        truc = X_df_new[pd.isnull(X_df['Age'])]
+        def fage(x):
+            return table.loc[x['Parch'],x['SibSp']]
+        
+# Replace missing values
+        X_df_new=X_df_new['Age'].fillna(truc.apply(fage, axis=1))   
+             
+        #X_df_new = X_df_new['Age'].fillna(X_df_new['Age'].mean())
         XX = X_df_new.values
         return XX
+
+        
+        
